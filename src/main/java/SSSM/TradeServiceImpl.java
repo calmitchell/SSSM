2698f6113d85cfed6947602acad522da1c4c6788
calmitchell <cal.mitchell@gmail.com>
2017-03-26 17:24:44 +0100

    Update TradeServiceImpl.java
    Added exception handling.

--- src/main/java/SSSM/TradeServiceImpl.java
+++ src/main/java/SSSM/TradeServiceImpl.java
@@ -32,9 +32,15 @@ public class TradeServiceImpl implements TradeService {
 	 * @param type Whether the trade is BUY or SELL.
 	 * @param price The price the trade was booked at.
 	 */
-	public void addTrade(String securitySymbol, LocalDateTime timestamp, int quantity, TradeType type, double price) {
+	
+	public void addTrade(String securitySymbol, LocalDateTime timestamp, int quantity, TradeType type, double price) throws StockNotFoundException {
 		Trade trade = new Trade(timestamp, quantity, type, price);
-		stockRepository.getStock(securitySymbol).addTrade(trade);
+		Stock stock = stockRepository.getStock(securitySymbol);
+		if(stock == null) {
+			throw new StockNotFoundException("Stock not found in repository to add trade");
+		}
+		
+		stock.addTrade(trade);
 	}
 
 	/** Get trade from the appropriate stock.
@@ -44,8 +50,12 @@ public class TradeServiceImpl implements TradeService {
 	 * 
 	 * @return Trade The trade from the stock repository. 
 	 * 	 */
-	public Trade getTrade(String securitySymbol,LocalDateTime timestamp) {
-		return stockRepository.getStock(securitySymbol).getTrades().getTrade(timestamp);
+	public Trade getTrade(String securitySymbol,LocalDateTime timestamp) throws StockNotFoundException {
+		Stock stock =stockRepository.getStock(securitySymbol);
+		if(stock == null) {
+			throw new StockNotFoundException("Stock not found in repository to add trade");
+		}
+		return stock.getTrades().getTrade(timestamp);
 	}
 	
 	/** Get the last 15 minutes of trade activity for appropriate stock from the stock repository.
@@ -53,8 +63,12 @@ public class TradeServiceImpl implements TradeService {
 	 * @param securitySymbol A string containing the securities symbol.
 	 * @return Collection A collection of the last 15 minutes of trade activity for stock.
 	 */
-	public Collection<Trade> getLast15MinutesTrades(String securitySymbol) {
-		return stockRepository.getStock(securitySymbol).getTrades().getLast15MinutesTrades();
+	public Collection<Trade> getLast15MinutesTrades(String securitySymbol) throws StockNotFoundException {
+		Stock stock =stockRepository.getStock(securitySymbol);
+		if(stock == null) {
+			throw new StockNotFoundException("Stock not found in repository to add trade");
+		}
+		return stock.getTrades().getLast15MinutesTrades();
 	}
 
 }
