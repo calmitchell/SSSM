4055c9556f12ca076fb554729647b73e47b9e920
calmitchell <cal.mitchell@gmail.com>
2017-03-26 17:23:50 +0100

    Added exception.

---
+++ src/main/java/SSSM/StockNotFoundException.java
@@ -0,0 +1,14 @@
+package SSSM;
+
+public class StockNotFoundException extends Exception {
+
+	public StockNotFoundException(String message) {
+		super(message);
+	}
+
+	/** Class for when stock is not found in simple stock market.
+	 * 
+	 */
+	private static final long serialVersionUID = 23L;
+
+}
